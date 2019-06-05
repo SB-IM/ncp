6328b3d022971bdc004906c2d3ecaed54ee00795
a-wing <1@233.email>
2019-06-05 21:02:32 +0800

    Add debuild.sh

---
+++ debuild.sh
@@ -0,0 +1,68 @@
+
+pkgname=ncp
+pkgver=0.1.0
+#arch=arm
+arch=amd64
+
+srcdir=${pkgname}_${pkgver}_linux_${arch}
+
+GOOS=linux GOARCH=${arch} go build
+
+mkdir -p debian
+
+cat > debian/rules << EOF
+#!/usr/bin/make -f
+%:
+	dh \$@
+
+EOF
+
+cat > debian/control << EOF
+Source: ncp
+Maintainer: a-wing <1@233.email>
+Build-Depends: debhelper (>= 8.0.0), golang (>= 1.11)
+Standards-Version: 3.9.3
+Section: utils
+
+Package: ncpgo
+Priority: extra
+Architecture: ${arch}
+Description: Go Node control protocol
+
+Package: ncpcmd
+Priority: extra
+Architecture: ${arch}
+Depends: ncpgo
+Description: Node control protocol
+EOF
+
+cat > debian/ncpgo.install << EOF
+ncpgo usr/bin/
+ncpgo.service lib/systemd/system/
+config-dist.yml etc/ncp/
+EOF
+
+cat > debian/ncpcmd.install << EOF
+ncp.rb usr/lib/ncp/
+Gemfile* usr/lib/ncp/
+lib usr/lib/ncp/
+scripts usr/lib/ncp/
+log usr/lib/ncp/
+
+ncp.service lib/systemd/system/
+EOF
+
+cat > debian/changelog << EOF
+${pkgname} (${pkgver}-1) unstable; urgency=low
+
+  * Initial release.
+
+ -- a-wing <1@233.email>  Sun, 07 Apr 2019 22:07:56 +0800
+EOF
+
+echo 9 > debian/compat
+
+debuild -us -uc
+
+rm -r debian
+
