741f94fd999a0f8bec0c504c55cbb986da67855d
a-wing <1@233.email>
2020-02-13 13:51:05 +0800

    Improve && go fmt util.go

--- util.go
+++ util.go
@@ -6,18 +6,17 @@ import (
 )
 
 type DuplicateFilter struct {
-  Msg string
+	Msg string
 }
 
-func (last *DuplicateFilter) Put(m string) (string) {
-  if last.Msg == m {
-    return ""
-  } else {
-    last.Msg = m
-    return m
-  }
+func (last *DuplicateFilter) Put(m string) string {
+	if last.Msg == m {
+		return ""
+	} else {
+		last.Msg = m
+		return m
+	}
 }
-
 
 func Ucfirst(str string) string {
 	for i, v := range str {
@@ -35,13 +34,12 @@ func Lcfirst(str string) string {
 
 // This function mainly solves the case where the number of bytes in a single line is greater than 4096
 // Reference: https://www.jianshu.com/p/5b686a9a8eed
-func readLine(r *bufio.Reader) (string, error) {
+func readLine(r *bufio.Reader) ([]byte, error) {
 	line, isprefix, err := r.ReadLine()
 	for isprefix && err == nil {
 		var bs []byte
 		bs, isprefix, err = r.ReadLine()
 		line = append(line, bs...)
 	}
-	return string(line), err
+	return line, err
 }
-
