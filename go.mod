315f15936620c06545ae80c88f61530f2336ee14
a-wing <1@233.email>
2020-10-09 12:24:39 +0800

    Fix (ncpio): test

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module sb.im/ncp
 go 1.13
 
 require (
+	github.com/SB-IM/jsonrpc-lite v0.1.0
 	github.com/SB-IM/jsonrpc2 v0.0.0-20200103125104-382421846c30
 	github.com/eclipse/paho.mqtt.golang v1.2.0
 	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
--- ncpio/ncpios_test.go
+++ ncpio/ncpios_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 	"time"
+
+	"github.com/SB-IM/jsonrpc-lite"
 )
 
 func TestNcpIOs(t *testing.T) {
@@ -16,13 +18,13 @@ func TestNcpIOs(t *testing.T) {
 			},
 			ORules: []Rule{
 				Rule{`.*"result".*`, true},
+				Rule{`.*`, false},
 			},
 		},
 		Config{
 			Type:   "jsonrpc2",
 			Params: "233",
 			IRules: []Rule{
-				//Rule{`.*"result".*`, true},
 				Rule{`.*`, false},
 			},
 			ORules: []Rule{
@@ -49,5 +51,47 @@ func TestNcpIOs(t *testing.T) {
 	}
 	if data := string(<-O); data == test_2 {
 		t.Error(data)
+	}
+}
+
+func TestNcpIOsMultipleRules(t *testing.T) {
+	configs := []Config{
+		Config{
+			Type:   "api",
+			Params: "233",
+			IRules: []Rule{
+				Rule{`.*`, false},
+			},
+			ORules: []Rule{
+				Rule{`.*"method": ?"webrtc".*`, true},
+				Rule{`.*`, false},
+			},
+		},
+		Config{
+			Type:   "jsonrpc2",
+			Params: "233",
+			IRules: []Rule{
+				Rule{`.*`, false},
+			},
+			ORules: []Rule{
+				Rule{`.*`, false},
+			},
+		},
+	}
+
+	ncpios := NewNcpIOs(configs)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go ncpios.Run(ctx)
+	time.Sleep(3 * time.Millisecond)
+
+	test_1 := `{"jsonrpc":"2.0","id":"test.0-0","method":"webrtc","params":[]}`
+	test_2 := `{"jsonrpc":"2.0","id":"xxx","method":"dooropen","params":[]}`
+
+	I <- []byte(test_1)
+	I <- []byte(test_2)
+
+	if data := <-O; jsonrpc.ParseObject(data).ID.Name != "xxx" {
+		t.Errorf("%s\n", data)
 	}
 }
