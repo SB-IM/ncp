e390850099ca03b96b04d05c73293b3b1f2cf2f3
a-wing <1@233.email>
2020-12-22 20:19:29 +0800

    Fix (ncpio): tcps Listener.Close()

--- ncpio/tcps.go
+++ ncpio/tcps.go
@@ -24,6 +24,11 @@ func (t *Tcpc) Listen(ctx context.Context) {
 			go t.send(conn)
 			go t.recv(conn)
 			select {
+			case <-ctx.Done():
+				if err := ln.Close(); err != nil {
+					logger.Println("Connect Close error:", err)
+				}
+				return
 			case <-t.ctx.Done():
 				conn.Close()
 				logger.Println("Connect err try reconnect")
