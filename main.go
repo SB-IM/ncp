3a4fccb5203004fefc248cd406ecc67452798b14
a-wing <1@233.email>
2020-12-30 11:12:04 +0800

    Add (main): v2 Launcher

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"sb.im/ncp/history"
 	C "sb.im/ncp/constant"
 
+	cfg "sb.im/ncp/tests/help"
+
   mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -285,7 +287,7 @@ func ncpCmd(ncp *NcpCmd, raw string) string {
 	return `{"jsonrpc":"2.0",`+s+`,"id":"` + rpc.Id + `"}`
 }
 
-func main() {
+func mainOld() {
 	config_path := "config.yml"
 
 	help := flag.Bool("h", false, "this help")
@@ -334,3 +336,46 @@ func main() {
 	msgCenter(c, config.Server, &config.NcpIO, &ncpCmd, config.Ncp, &config.Log)
 }
 
+func main() {
+	config_path := "config.yml"
+
+	help := flag.Bool("h", false, "this help")
+	flag.StringVar(&config_path, "c", "config.yml", "set configuration file")
+
+	show_version := flag.Bool("v", false, "show version")
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		return
+	}
+
+	if *show_version {
+		fmt.Printf("Ncp %s %s %s %s\n", C.Version, runtime.GOOS, runtime.GOARCH, C.BuildTime)
+		return
+	}
+
+	if os.Getenv("NCP_CONF") != "" {
+		config_path = os.Getenv("NCP_CONF")
+	}
+	log.Println("load config: " + config_path)
+
+	config, err := cfg.GetConfig(config_path)
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Printf("%+v", config)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go ncpio.NewNcpIOs(config.NcpIO).Run(ctx)
+
+	// Wait mqttd server startup && sub topic on broker
+	time.Sleep(3 * time.Millisecond)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	<-sigs
+	log.Println("ncpio exit")
+}
